d22b98a4c01d98f0a96864a6d06708434ccd7104
RAdKuzur <drive16052003@gmail.com>
2025-05-28 22:35:08 +0400

    prepare applications

---
+++ backend/controllers/admin/ApplicationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace backend\controllers\admin;
+
+use yii\web\Controller;
+
+class ApplicationController extends Controller
+{
+    public function actionIndex(){
+        return $this->render('index');
+    }
+    public function actionCreate(){
+
+    }
+    public function actionUpdate($id){
+
+    }
+    public function actionDelete($id){
+
+    }
+}
